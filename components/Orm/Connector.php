208ea903198f2deef3790673f5fd979dde98d964
FameTiger <Kruassan858@gmail.com>
2020-12-29 00:28:31 +0200

    f

--- components/Orm/Connector.php
+++ components/Orm/Connector.php
@@ -4,44 +4,40 @@ namespace Components\Orm;
 
 class Connector
 {
-	protected $config;
-	protected $configPatch = [
-	'test' => '..app/Config/configDB.php'
-	];
-	public function __construct(string $objName = 'test')
-	{
-		if(array_key_exist($dbName, $this->configPatch)){
-			
-			$this->config = require( $this->configPatch[$objName]);
-	}
-		else
-		{
-			throw new \Exception('DBName not value');
-	}
-	}
+    protected $config;
+    protected $configPatch = [
+        'test' => '..app/Config/configDB.php',
+    ];
 
-public function connect()
-{
-	if(empty(this->config['dbName']){
-	{)
-		throw \Exception ('Bad dbName');
-	}
-		if(empty(this->config['host'])
-		{
-			throw new \Exception('Bad host');
-		}
+    public function __construct(string $objName = 'test')
+    {
+        if (array_key_exists($objName, $this->configPatch)) {
+            $this->config = require $this->configPatch[$objName];
+        } else {
+            throw new \Exception('DBName not value');
+        }
+    }
 
-	if(empty(this->config['user']){
-	{
-		throw new \Exception('Bad user');
-	}
-		if(empty(this->config['pass']){
-		{
-			throw \Exception('Bad pass');
+    public function connect()
+    {
+        if (empty($this->config['dbName'])) {
+            throw new \Exception('Bad dbName');
+        }
+
+        if (empty($this->config['host'])) {
+            throw new \Exception('Bad host');
+        }
+
+        if (empty($this->config['user'])) {
+            throw new \Exception('Bad user');
+        }
+
+        if (empty($this->config['pass'])) {
+            throw new \Exception('Bad pass');
+        }
+
+        $dns = $this->config['driver'] . ':host=' . $this->config['host'] . ';dbname=' . $this->config['dbName'];
+        return new \PDO($dns, $this->config['user'], $this->config['pass']);
+    }
 }
-	$dns = $this->config['driver'].':host='.$this->config['host'].';dbname='.$this->['dbName'];
-	return new \PDO($dns,$this->config['user'], $this->config['pass']);
-}
- 
- //мужик, как я это должен сделать по твоему? Я же даун, алло. Оно не работает. 
 	

880989ad04f6596621e2494dd8569acf0c95cb49
FameTiger <Kruassan858@gmail.com>
2021-01-16 18:53:24 +0200

    f

--- core/Router.php
+++ core/Router.php
@@ -14,11 +14,12 @@ class Router
     {
         $this->routingMap = include_once '../app/Config/Conf.php';
         $this->requestPatch = $_SERVER['PATH_INFO'] ?? '/';
-		parse_str($_SERVER['QUERY_STRING'],$this->query);
+        parse_str($_SERVER['QUERY_STRING'], $this->query);
     }
 
     public function run()
     {
+
         if(in_array($this->requestPatch, array_keys ($this->routingMap))) {
             if (strpos($this->requestPatch, '/admin') === 0) {
                 $classNamespace = 'App\\Controllers\\Admin\\' . $this->routingMap[$this->requestPatch] . 'Controller';
@@ -28,9 +29,27 @@ class Router
         } else {
             $classNamespace = 'App\\Controllers\\NotFoundController';
         }
+        
+        $action = 'index';
+        $requestParts = explode('/', $this->requestPatch);
+        if (strpos($this->requestPatch, '/admin') === 0) {
+            if (isset($requestParts[3])) {
+                $action = $requestParts[3];
+            }
+        } else {
+            if (isset($requestParts[2])) {
+                $action = $requestParts[2];
+            }
+        }
 
-		$action = $this->query['action'] ?? 'index';
         $classObj = new $classNamespace();
-        $classObj->$action();
+        
+        if (method_exists($classObj, $action)) {
+            $classObj->$action($this->query);
+        } else {
+            $classNamespace = 'App\\Controllers\\NotFoundController';
+            $classObj = new $classNamespace();
+            $classObj->index($this->query);
+        }
     }
 }
